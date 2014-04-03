85a2b7262c5bee2fc88bf2481f3bcc0654543828
szwagier90 <szwagier90@gmail.com>
2014-04-02 18:01:24 -0700

    Stworzone modele obiektow bazy danych

--- project/android_obd/models.py
+++ project/android_obd/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tag(models.Model):
+	name = models.CharField(max_length=50)
+
+class Record(models.Model):
+	user = ForeignKey(User)
+	video_link = models.URLField(max_length=200, blank=True)
+	tags = models.ManyToManyField(Tag)
+
+class MeasurementType(models.Model):
+	name = models.CharField(max_length=100)
+	description = models.TextField()
+
+class Measurement(models.Model):
+	record = models.ForeignKey(Record)
+	measurement_type = models.ForeignKey(MeasurementType)
+	timestamp = models.DateTimeField()
+	value = models.BinaryField()
