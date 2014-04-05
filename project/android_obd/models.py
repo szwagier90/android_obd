c744427e9b9033a3dda214a8463709d859a3a8b2
szwagier90 <szwagier90@gmail.com>
2014-04-05 16:27:04 -0700

    Dodane metody __unicode__ w klasach modeli

--- project/android_obd/models.py
+++ project/android_obd/models.py
@@ -4,17 +4,29 @@ from django.contrib.auth.models import User
 class Tag(models.Model):
 	name = models.CharField(max_length=50)
 
+	def __unicode__(self):
+		return self.name
+
 class Record(models.Model):
-	user = ForeignKey(User)
+	user = models.ForeignKey(User)
 	video_link = models.URLField(max_length=200, blank=True)
 	tags = models.ManyToManyField(Tag)
+
+	def __unicode__(self):
+		return "%s: %s (%s)" % (self.user, self.tags.all(), self.video_link)
 
 class MeasurementType(models.Model):
 	name = models.CharField(max_length=100)
 	description = models.TextField()
+
+	def __unicode__(self):
+		return "%s: %s" % (self.name, self.description)
 
 class Measurement(models.Model):
 	record = models.ForeignKey(Record)
 	measurement_type = models.ForeignKey(MeasurementType)
 	timestamp = models.DateTimeField()
 	value = models.BinaryField()
+
+	def __unicode__(self):
+		return "%s: %s" % (self.timestamp, self.measurement_type.name)
