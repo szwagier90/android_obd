a34a05a910776c67081d29c00ce8b13fea79857a
szwagier90 <szwagier90@gmail.com>
2014-04-05 16:35:26 -0700

    Ustawiona podstawowa konfiguracja plikow urls i dodany pierwszy widok dla index

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,12 +1,9 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+	url(r'^android_obd/', include('android_obd.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
