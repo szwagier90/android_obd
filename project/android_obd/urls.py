e548760b63b2218415516e3e177df1524e3ac5e6
szwagier90 <szwagier90@gmail.com>
2014-04-13 13:55:07 -0700

    Wstepna wersja strony domowej

--- project/android_obd/urls.py
+++ project/android_obd/urls.py
@@ -2,5 +2,10 @@ from django.conf.urls import patterns, url
 from android_obd import views
 
 urlpatterns = patterns('',
-	url(r'', views.index, name='index'),
+	url(r'^$', views.index, name='index'),
+	url(r'^register/$', views.index, name='register'),
+	url(r'^register/$', views.index, name='more_recent'),
+	url(r'^register/$', views.index, name='more_added'),
+	url(r'^register/$', views.index, name='more_longest'),
+	url(r'^register/$', views.index, name='more_fuel'),
 )
--- project/android_obd/views.py
+++ project/android_obd/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-	return render(request, 'android_obd/base.html', {})
+	return render(request, 'android_obd/home.html', {})
