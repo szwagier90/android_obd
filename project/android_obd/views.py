706ec82e874c73509b9abd7847acd8668ed7eb0b
piotut <piotut@gmail.com>
2014-04-18 11:04:23 -0700

    views.py dodane przykladowe dane map.js poprawiony kod pod baze

--- project/android_obd/views.py
+++ project/android_obd/views.py
@@ -8,10 +8,16 @@ def index(request):
 def route(request):
 	#slownik = {"wsp": [['Y',-121.44],['X',37.78]]}
 	#slownik = {"wsp": [['Y',-121.4],['X',37.7699298]]}
-	
+
 	lista =  [
-		{'X':37.60,'Y':-121.44},{'X':37.7699298}
-	]
+        {'X':37.60,'Y':-121.44,'spalanie':0,'predkosc':0},
+        {'X':37.7699298,'Y':-118.4469157,'spalanie': 4,'predkosc':78},
+        {'X':36.7699298,'Y':-120.4469157,'spalanie': 7,'predkosc':56},
+        {'X':38.7699298,'Y':-120.4469157,'spalanie': 8,'predkosc':30},
+        {'X':37.7699298,'Y':-122.4469157,'spalanie':12},
+        {'X':40.7699298,'Y':-122.4469157,'spalanie':21,'predkosc':185}
+        ]
+
 	#slownik = {"wsp":[-121.44, 37.77]}
 	json_list = simplejson.dumps(lista)
 	
